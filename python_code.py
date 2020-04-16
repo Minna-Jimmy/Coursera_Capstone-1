c22ee22e3044d84d097deb3d570bb39ed04fbaea
shivamvns <shivamsrivastava033@gmail.com>
2020-04-16 09:55:15 +0530

    first commit

---
+++ python_code.py
@@ -0,0 +1,7 @@
+##This is the first notebook of Applied Data Science Capstone Course for Peer-graded Assignment
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
